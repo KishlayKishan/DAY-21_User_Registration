a03dd3238954c83c04f7f70a8397e4d1cc7cad18
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-16 09:00:57 +0530

    Addedupto uc11 from day20_user registration

---
+++ src/main/java/com/bridgelabz/userregistration/UserRegistrationException.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistrationException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserRegistrationException(String message) {
+		super(message);
+	}
+}
